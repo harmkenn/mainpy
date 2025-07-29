cd360c36bd25edfcfd04dee1327f348702d58f77
Ken Harmon <harmkenn@gmail.com>
2025-07-29 09:17:29 +0000

    Update

--- main.py
+++ main.py
@@ -21,20 +21,25 @@ sub_apps_folder = 'apps'
 sub_apps = [f for f in os.listdir(sub_apps_folder) if f.endswith('.py')]
 
 # Create radio buttons in the sidebar using the user-friendly names
-selected_sub_app_name = st.sidebar.radio('Select a sub-app', list(sub_app_names.values()))
+selected_sub_app_name = st.sidebar.radio('Select a sub-app',
+                                         list(sub_app_names.values()))
 
 # Get the corresponding .py filename from the selected name
-selected_sub_app = [k for k, v in sub_app_names.items() if v == selected_sub_app_name][0]
+selected_sub_app = [
+    k for k, v in sub_app_names.items() if v == selected_sub_app_name
+][0]
 
 # Sidebar input for comma-separated tickers
-tickers_input = st.sidebar.text_area(
-    "Enter comma-separated stock tickers",
-    value=st.session_state.get("tickers", "MSTY,TSLY,NVDY,CONY,\nMAIN,ULTY"),height=100
-)
+tickers_input = st.sidebar.text_area("Enter comma-separated stock tickers",
+                                     value=st.session_state.get(
+                                         "tickers",
+                                         "MSTY,TSLY,NVDY,CONY,\nMAIN,ULTY"),
+                                     height=100)
 st.session_state["tickers"] = tickers_input
 
 # Import and run the selected sub-app
 if selected_sub_app:
-    spec = importlib.util.spec_from_file_location(selected_sub_app, os.path.join(sub_apps_folder, selected_sub_app))
+    spec = importlib.util.spec_from_file_location(
+        selected_sub_app, os.path.join(sub_apps_folder, selected_sub_app))
     sub_app_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(sub_app_module)
